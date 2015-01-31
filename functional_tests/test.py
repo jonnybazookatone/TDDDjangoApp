f0528b5eb5cd6d1d72b9003389c0ade1a7609a43
Jonny Elliott <jonnynelliott@googlemail.com>
2015-01-31 16:58:08 -0500

    Corrected setupclass and teardownclass for when we provide a live server to test against

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -5,6 +5,20 @@ from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass(cls)
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -22,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # The user has heard about a really new and cool website and so goes to investigate it
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # They notice the title of the webpage mentions to-do lists
         # import time
@@ -65,7 +79,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
 
         # They visit the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -89,7 +103,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_styling(self):
         # The user goe to the web page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # The user notices the input box is centred
