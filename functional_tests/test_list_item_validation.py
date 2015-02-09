b8b0463a165b879533f36ad59801664acee3cf1b
Jonny Elliott <jonnynelliott@googlemail.com>
2015-02-09 09:59:34 -0500

    First FT fails for the inclusion of javascript code that will remove error messages when the user addresses them.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -5,6 +5,9 @@ from lists.forms import DUPLICATE_ITEM_ERROR
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
 
         # The user goes to the home page and accidentally tries to submit
@@ -14,7 +17,7 @@ class ItemValidationTest(FunctionalTest):
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, 'You cannot have an empty list item')
 
         # The user tries again with some text for the item, which now works
@@ -26,7 +29,7 @@ class ItemValidationTest(FunctionalTest):
 
         # The user receives a similar warning on the list page
         self.check_for_row_in_list_table('1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, 'You cannot have an empty list item', error.text)
 
         # The user can correct it by filling some text in
@@ -44,7 +47,23 @@ class ItemValidationTest(FunctionalTest):
 
         # The user accidentally tries to enter a duplicate item
         self.get_item_input_box().send_keys('Buy wellies\n')
-        error = self.browser.find_element_by_css_selector('.has-error')
-        self.assertEqual(error.text, DUPLICATE_ITEM_ERROR)
 
         # The user receives a helpful message that informs them
+        # of their mistake
+        error = self.get_error_element()
+        self.assertEqual(error.text, DUPLICATE_ITEM_ERROR)
+
+    def test_error_messages_are_cleared_on_input(self):
+
+        # User starts a new list in a way that causes a validation error
+        self.browser.get(self.server_url)
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # The user starts typing in the input box to clear the error
+        self.get_item_input_box().send_keys('a')
+
+        # The user is pleased to see that the error message dissapears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
